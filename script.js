f3e67a3bddf3d9682b76866149c34fd2dfbc1aa4
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-09 08:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function showData(event) {
+	event.preventDefault();
+
+	let first=document.getElementById("firstName").value;
+	let last=document.getElementById("LastName").value;
+	let phone=document.getElementById("phone").value;
+	let email=document.getElementById("email").value;
+
+	alert(
+		"First Name: " + first + "Last Name: " + last + "Phone Number: " + phone + "Email ID: " + email
+	);
+}
